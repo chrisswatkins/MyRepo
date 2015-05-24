50572741454960a0060787a9a31dc6a697a7b962
Chris Watkins <chrisswatkins@earthlink.net>
2015-05-24 10:23:09 -0400

    Final documentation
    Final documentation

--- ProgrammingAssignment2-master/cachematrix.R
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -2,15 +2,16 @@
 ## state in R programs
 
 
-## This function creates a special "matrix" object that prodices a list of 3 functions.
+## This function creates a special "matrix" object that produces a list of 5 functions.
+## that can be utilized by a calling function to obtain cached info
 
 makeCacheMatrix <- function(x = matrix()) {
 
         invmat <- NULL                             ## initialize inverse variable
         datastore <- NULL                          ## initialize input data
         
-        setcachedata <- function(z) datastore <<- z
-        getcachedata <- function() datastore 
+        setcachedata <- function(z) datastore <<- z  ## Store input data set
+        getcachedata <- function() datastore         ## Get cached data set from prior run
         get <- function() x                        ## Create function to get input matrix
         setmatrixinv <- function(y) invmat <<- y   ## Create function to set the result of matrix inverse
         getmatrixinv <- function() invmat          ## Create function to get the result of matrix inverse
@@ -30,17 +31,22 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
-        datastore <- NULL                       ## initialize cached input data
+        ##datastore <- NULL                       ## initialize cached input data
 
-        m <- x$getmatrixinv()                   ## test for cached calculated inverse
+        m_inv <- x$getmatrixinv() 
+        prior_data <- x$getcachedata()
+        current_data <- x$get()
         
-        if(!is.null(m) || identical(x$getcachedata(), x$get() )  ) {                       ## Check for NULL or changed input
+        ## test for cached calculated inverse
+        ## or existance of same input data matrix
+        
+        if(!is.null(m_inv) || identical(prior_data, current_data )  ) {                       ## Check for NULL or changed input
                 message("getting cached data")
-                return(m)
+                return(m_inv)
         }
 
         data <- x$get()                         ## obtain input data
-        x$setcachedata(data)                       ## cache input data
+        x$setcachedata(data)                    ## cache input data for future compare
         sol <- solve(data, ...)                 ## solve for inverse
         x$setmatrixinv(sol)                     ## cache inverse 
         sol                                     ## return inverse
