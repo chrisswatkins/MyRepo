00f1d692b547821aa8a6254449c816123612213f
Chris Watkins <chrisswatkins@earthlink.net>
2015-05-25 14:34:46 -0400

    Latest

--- ProgAssign3/best.R
+++ ProgAssign3/best.R
@@ -26,7 +26,7 @@ best <- function (in_state, in_outcome) {
 
                 }  ## end for loop
         
-        if (!good_state){stop("invalid state")} else {print("valid state")}
+        if (!good_state){stop("invalid state")} 
         
         ## Check that in_outcome is valid        
 
@@ -40,17 +40,25 @@ best <- function (in_state, in_outcome) {
                 
                 if (in_outcome == ValidOutcome[i]) {
                         good_outcome <- as.logical("TRUE")
+                        
+                        ## Set column num of selected outcome
+                        if (i == 1){DataIndex <- 11}
+                        if (i == 2){DataIndex <- 17}
+                        if (i == 3){DataIndex <- 23}
                         break
                 }
                 
         }  ## end for loop
         
-        if (!good_outcome){stop("invalid outcome")} else {print("valid outcome")}
+        if (!good_outcome){stop("invalid outcome")} 
         
-        
-        
+                
         ## Return Hospital name in that state with the lowest 30-day death rate
-        
+        state_set <- outcome[outcome$State == in_state, c(2,DataIndex)]
+        state_set[,2] <- as.numeric(state_set[,2])
+        OrderedSet <- state_set[order(state_set[2], state_set[1]),]
+        ans <- OrderedSet[1,1]
+        ans
         
 }
         
